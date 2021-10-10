295f0092323fada67d69161b4a4c6d8b9576c97c
Dang Truong Giang <giangdt.kma@gmail.com>
2021-10-10 17:59:00 +0700

    update ui

--- js.js
+++ js.js
@@ -1,20 +1,17 @@
 $(".tab_content").hide();
 $(".tab_content:first").show();
 
-$("ul.tabs li a").click(function () {
-  const index = $(this).attr("index");
-  const tabClicked = $(`ul.tabs li:nth-child(${index})`);
-
+$("ul.tabs li").click(function () {
   // we will no longer activate this tab once it has been activated.
-  const isTabActived = tabClicked.attr("class") === "active";
+  const isTabActived = $(this).attr("class") === "active";
   if (isTabActived) return;
 
   // active tab
   $(".tab_content").hide();
-  var activeTab = tabClicked.attr("rel");
+  var activeTab = $(this).attr("rel");
   $("#" + activeTab).fadeIn();
   $("ul.tabs li").removeClass("active");
-  tabClicked.addClass("active");
+  $(this).addClass("active");
   $(".tab_drawer_heading").removeClass("d_active");
   $(".tab_drawer_heading[rel^='" + activeTab + "']").addClass("d_active");
 });
